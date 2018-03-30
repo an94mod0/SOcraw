8f99f08619e4f5c60332a2dbe0095700921c0e67
an94mod0 <an94mod0@gmail.com>
2018-03-30 18:07:54 +0800

    finish GetTags

--- GetTags.py
+++ GetTags.py
@@ -1,8 +1,10 @@
 import requests
 import pymysql
 import json
+import time
 psize=100
 page=1
+endpage=100
 def apostrophed(text):
     new_text=""
     for c in text:
@@ -14,7 +16,7 @@ me=json.loads(open('account.json',encoding = 'utf8').read().encode('utf8'))
 db = pymysql.connect(me['host'],me['username'],me['password'],me['db'],use_unicode=True, charset="utf8mb4")
 cursor = db.cursor()
 while True:
-    if page==2: break
+    #print(page)
     APIurl="https://api.stackexchange.com/2.2/tags?page="+str(page)+"&pagesize="+str(psize)
     APIurl+="&order=desc&sort=popular&site=stackoverflow"
     text=requests.get(APIurl).json()
@@ -22,7 +24,13 @@ while True:
         count=1
         command="INSERT INTO `knkn`.`tags` VALUES (NULL, '"+apostrophed(x['name'])+"', '"+str(x['count'])+ "');"
         cursor.execute(command)
+    if page==endpage:
+        break
     if text['has_more']:
         page+=1
+        if 'backoff' in text:
+            time.sleep(text['backoff'])
     else:
         break
+db.commit()
+db.close()
